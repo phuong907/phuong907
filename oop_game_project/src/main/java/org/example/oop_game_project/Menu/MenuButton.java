000af4a4cd33e8d534fcd1fb6d1f3cfefd52e2ed
Minh701 <letheminh070104@gmail.com>
2025-05-03 18:14:54 +0700

    Minh701

--- oop_game_project/src/main/java/org/example/oop_game_project/Menu/MenuButton.java
+++ oop_game_project/src/main/java/org/example/oop_game_project/Menu/MenuButton.java
@@ -1,4 +1,25 @@
 package org.example.oop_game_project.Menu;
 
-public class MenuButton {
+import com.almasb.fxgl.dsl.FXGL;
+import javafx.beans.binding.Bindings;
+import javafx.scene.Parent;
+import javafx.scene.paint.Color;
+
+public class MenuButton extends Parent {
+    MenuButton(String name, double fontSize, Runnable action) {
+        var text = FXGL.getUIFactoryService().newText(name, Color.WHITE, fontSize);
+        text.setStrokeWidth(1.5);
+        text.strokeProperty().bind(text.fillProperty());
+
+        text.fillProperty().bind(
+                Bindings.when(hoverProperty())
+                        .then(Color.rgb(248, 185, 54))
+                        .otherwise(Color.WHITE)
+        );
+
+        setOnMouseClicked(e -> action.run());
+
+        setPickOnBounds(true);
+        getChildren().add(text);
+    }
 }
