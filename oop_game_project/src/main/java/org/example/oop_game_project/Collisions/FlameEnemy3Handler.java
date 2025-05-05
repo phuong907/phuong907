4c83993ead22ac197ada08fc85023db3475f3712
Minh701 <letheminh070104@gmail.com>
2025-05-05 11:44:29 +0700

    Minh701

--- oop_game_project/src/main/java/org/example/oop_game_project/Collisions/FlameEnemy3Handler.java
+++ oop_game_project/src/main/java/org/example/oop_game_project/Collisions/FlameEnemy3Handler.java
@@ -22,7 +22,32 @@ public class FlameEnemy3Handler extends CollisionHandler {
 
     @Override
     protected void onCollision(Entity flame, Entity enemy) {
+        if (flame.getComponent(FlameComponent.class).isActivation()
+                && enemy.getComponent(Enemy3.class).getState() != DIE) {
+            enemy.getComponent(Enemy3.class).setStateDie();
+            getGameTimer().runOnceAfter(() -> {
+                onTransform(enemy);
+                enemy.removeFromWorld();
+                set("enemies", getGameWorld().getGroup(ENEMY1,
+                        ENEMY2, ENEMY3, ENEMY4, ENEMY5).getSize());
+            }, Duration.seconds(2.4));
+        }
     }
 
     private void onTransform(Entity parent) {
+        Entity child1 = spawn("enemy1", new SpawnData(parent.getX(), parent.getY()));
+        Entity child2 = spawn("enemy1", new SpawnData(parent.getX(), parent.getY()));
+
+        if (parent.getComponent(Enemy3.class).getDx() == 0) {
+            child1.getComponent(Enemy1.class).setState(UP);
+            child1.getComponent(Enemy1.class).setDxDy(0, -ENEMY_SPEED);
+            child2.getComponent(Enemy1.class).setState(DOWN);
+            child2.getComponent(Enemy1.class).setDxDy(0, ENEMY_SPEED);
+        } else if (parent.getComponent(Enemy3.class).getDy() == 0) {
+            child1.getComponent(Enemy1.class).setState(LEFT);
+            child1.getComponent(Enemy1.class).setDxDy(-ENEMY_SPEED, 0);
+            child2.getComponent(Enemy1.class).setState(RIGHT);
+            child2.getComponent(Enemy1.class).setDxDy(ENEMY_SPEED, 0);
+        }
+    }
 }
--- oop_game_project/src/main/java/org/example/oop_game_project/Components/Enemy/Enemy1.java
+++ oop_game_project/src/main/java/org/example/oop_game_project/Components/Enemy/Enemy1.java
@@ -1,4 +1,38 @@
 package org.example.oop_game_project.Components.Enemy;
 
-public class Enemy1 {
+import org.example.oop_game_project.Components.FlameComponent;
+import javafx.util.Duration;
+
+import static org.example.oop_game_project.Constants.Constant.ENEMY_SPEED;
+import static org.example.oop_game_project.GameType.*;
+import static com.almasb.fxgl.dsl.FXGL.*;
+
+public class Enemy1 extends Enemy {
+    public Enemy1() {
+        super(-ENEMY_SPEED, 0, 1, 3, "enemy1.png");
+
+        onCollisionBegin(ENEMY1, BRICK, (enemy1, brick) -> {
+            enemy1.getComponent(Enemy1.class).turn();
+        });
+        onCollisionBegin(ENEMY1, WALL, (enemy1, wall) -> {
+            enemy1.getComponent(Enemy1.class).turn();
+        });
+        onCollisionBegin(ENEMY1, DOOR, (enemy1, door) -> {
+            enemy1.getComponent(Enemy1.class).turn();
+        });
+        onCollisionBegin(ENEMY1, BOMB, (enemy1, bomb) -> {
+            enemy1.getComponent(Enemy1.class).turn();
+        });
+        onCollision(ENEMY1, FLAME, (enemy1, flame) -> {
+            if(flame.getComponent(FlameComponent.class).isActivation()) {
+                enemy1.getComponent(Enemy1.class).setStateDie();
+                getGameTimer().runOnceAfter(() -> {
+                    enemy1.removeFromWorld();
+                    set("enemies", getGameWorld().getGroup(ENEMY1,
+                            ENEMY2, ENEMY3, ENEMY4, ENEMY5).getSize());
+                }, Duration.seconds(2.4));
+            }
+        });
+    }
+
 }
